5b506d7150c18df3f0a887d88ea72f524472eb03
Mark Iannucci <mark.iannucci@gmail.com>
2018-10-12 03:50:03 -0600

    Remove curly braces

--- ChromeWebStoreBashScript.sh
+++ ChromeWebStoreBashScript.sh
@@ -1,4 +1,4 @@
-ACCESS_TOKEN=$(curl "https://accounts.google.com/o/oauth2/token" -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
+ACCESS_TOKEN=$(curl "https://accounts.google.com/o/oauth2/token" -d "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&refresh_token=$REFRESH_TOKEN&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
 
 curl -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-goog-api-version: 2" -X PUT -T ${AGENT_RELEASEDIRECTORY}/${RELEASE_PRIMARYARTIFACTSOURCEALIAS}/drop/EpicUserWebSearch.zip -v "https://www.googleapis.com/upload/chromewebstore/v1.1/items/$APP_ID"
 
