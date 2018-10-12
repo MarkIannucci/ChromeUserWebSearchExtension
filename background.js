9fcbf526742e3d60b00f57695b1935bc3b7a4a58
Mark Iannucci <mark.iannucci@gmail.com>
2018-10-11 23:07:27 -0600

    Enabled multiple section search

--- background.js
+++ background.js
@@ -1,11 +1,23 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-    // Encode user input for special characters , / ? : @ & = + $ #
-    var newURL = 'https://galaxy.epic.com/?#Search/searchWord=' + encodeURIComponent(text);
-    chrome.tabs.update({ url: newURL });
-  });
+    function (text) {
+
+        // if user enters a keyword after the omnibox keyword, redirect search to different destination
+        var splitText = text.split(' ');
+        var firstWord = splitText[0];
+
+        switch (firstWord) {
+            case 'sherlock':
+                var newURL = 'https://sherlock.epic.com/default.aspx?view=slg/search#txt=' + encodeURIComponent(text.substring(9));
+                break;
+            case 'topic':
+                var newURL = 'https://userweb.epic.com/Search?Query=' + encodeURIComponent(text.substring(6));
+                break;
+            case 'galaxy':
+                var newURL = 'https://galaxy.epic.com/?#Search/searchWord=' + encodeURIComponent(text.substring(6));
+                break;
+            default:
+                var newURL = 'https://galaxy.epic.com/?#Search/searchWord=' + encodeURIComponent(text);
+        }
+
+        chrome.tabs.update({ url: newURL });
+    });
