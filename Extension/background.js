cbd363f16bf0f5134fe15ceb0c4ce155af2216c5
Mark Iannucci <mark.iannucci@gmail.com>
2018-10-24 22:30:36 -0600

    Remove the alligators if the user enters them #8

--- Extension/background.js
+++ Extension/background.js
@@ -1,3 +1,10 @@
+function trimAlligators(searchText) {
+    if (searchText.slice(0, 1) == '<' && searchText.slice(-1) == '>') {
+        return searchText.slice(1, -1);
+    } else
+    { return searchText; }
+};
+
 chrome.omnibox.onInputEntered.addListener(
     function (text) {
 
@@ -9,19 +16,19 @@ chrome.omnibox.onInputEntered.addListener(
         switch (firstWord.toLowerCase()) {
             case 'sherlock':
                 if (isNaN(secondWord)) {
-                    var newURL = 'https://sherlock.epic.com/default.aspx?view=slg/search#txt=' + encodeURIComponent(text.substring(9));
+                    var newURL = 'https://sherlock.epic.com/default.aspx?view=slg/search#txt=' + encodeURIComponent(trimAlligators(text.substring(9)));
                 } else {
                     var newURL = 'https://sherlock.epic.com/default.aspx?view=slg/home#id=' + secondWord + '&view=1';
                 }
                 break;
             case 'topic':
-                var newURL = 'https://userweb.epic.com/Search?Query=' + encodeURIComponent(text.substring(6));
+                var newURL = 'https://userweb.epic.com/Search?Query=' + encodeURIComponent(trimAlligators(text.substring(6)));
                 break;
             case 'galaxy':
-                var newURL = 'https://galaxy.epic.com/?#Search/searchWord=' + encodeURIComponent(text.substring(6));
+                var newURL = 'https://galaxy.epic.com/?#Search/searchWord=' + encodeURIComponent(trimAlligators(text.substring(6)));
                 break;
             default:
-                var newURL = 'https://galaxy.epic.com/?#Search/searchWord=' + encodeURIComponent(text);
+                var newURL = 'https://galaxy.epic.com/?#Search/searchWord=' + encodeURIComponent(trimAlligators(text));
         }
 
         chrome.tabs.update({ url: newURL });
